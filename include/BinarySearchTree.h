063396f77aac785e52b7c2730487cab918d7c1bd
KonstantinPronin <prosayfer@mail.ru>
2016-09-27 21:54:05 +0300

    Update BinarySearchTree.h

--- include/BinarySearchTree.h
+++ include/BinarySearchTree.h
@@ -14,6 +14,9 @@ template <typename T>
 std::istream& operator >> (std::istream& in, BinarySearchTree<T>& tree);
 
 template <typename T>
+std::ifstream& operator >> (std::ifstream& fin, BinarySearchTree<T>& tree);
+
+template <typename T>
 class BinarySearchTree {
 	class Node {
 	public:
@@ -30,37 +33,50 @@ class BinarySearchTree {
 	}; 
 
 	Node* Root;
-	size_t size;
+	size_t Size;
 	
 public:
-	BinarySearchTree() : Root(nullptr), size(0) {};
-	BinarySearchTree(BinarySearchTree<T>&& tree);
+	BinarySearchTree() : Root(nullptr), Size(0) {};
 	BinarySearchTree(const std::initializer_list<T>& list);
-	bool add(T& val) noexcept;
-	const T* get(const T& val) const noexcept;
-	void Traverse(std::ostream& out, Node* root) const noexcept;
-	Node* ReturnRoot() const noexcept {return Root;}
-	BinarySearchTree<T>& operator= (BinarySearchTree<T>&& tree);
+	BinarySearchTree(const BinarySearchTree<T>& tree);
+	BinarySearchTree(BinarySearchTree<T>&& tree);
 	~BinarySearchTree();
+	
+	bool Insert(T& val) noexcept;
+	const T* Find(const T& val) const noexcept;
+	void Direct(std::ostream& out, Node* root) const noexcept;
+	void Symmetric(std::ostream& out, Node* root) const noexcept;
+	void Copy(Node* ptr) noexcept;
+	bool Compare(Node* ptr1, Node* ptr2) const noexcept;
+	Node* ReturnRoot() const noexcept {return Root;}
+	
+	BinarySearchTree<T>& operator= (const BinarySearchTree<T>& tree) noexcept;
+	BinarySearchTree<T>& operator= (BinarySearchTree<T>&& tree) noexcept;
+	bool operator== (const BinarySearchTree<T>& tree) const noexcept;
 };
 
 template <typename T>
-BinarySearchTree<T>::BinarySearchTree(const std::initializer_list<T>& list):size(0), Root(nullptr){
+BinarySearchTree<T>::BinarySearchTree(const std::initializer_list<T>& list){
 	for (auto it : list) {
-		add(it);
+		Insert(it);
 	}
 }
 
 template <typename T>
-BinarySearchTree<T>::BinarySearchTree(BinarySearchTree<T>&& tree):size(tree.size), Root(tree.Root){
+BinarySearchTree<T>::BinarySearchTree(const BinarySearchTree<T>& tree){
+	*this = tree;
+}
+
+template <typename T>
+BinarySearchTree<T>::BinarySearchTree(BinarySearchTree<T>&& tree):Size(tree.Size), Root(tree.Root){
 	tree.Root = nullptr;
 }
 
 template <typename T>
-bool BinarySearchTree<T>::add(T& val) noexcept {
-	if (!size) {
+bool BinarySearchTree<T>::Insert(T& val) noexcept {
+	if (!Size) {
 		Root = new Node(val);
-		size++;
+		Size++;
 		return true;
 	}
 	Node* CurPtr = Root;
@@ -71,7 +87,7 @@ bool BinarySearchTree<T>::add(T& val) noexcept {
 		if (CurPtr->value > val)
 			CurPtr = CurPtr->left;
 		else if (CurPtr->value < val)
-			CurPtr = CurPtr->right;
+ 			CurPtr = CurPtr->right;
 		else if (CurPtr->value == val)
 			return false;
 	}
@@ -79,12 +95,12 @@ bool BinarySearchTree<T>::add(T& val) noexcept {
 		PrevPtr->left = new Node(val);
 	else 
 		PrevPtr->right = new Node(val);
-	size++;
+	Size++;
 	return true;
 }
 
 template <typename T>
-const T* BinarySearchTree<T>::get(const T& val) const noexcept {
+const T* BinarySearchTree<T>::Find(const T& val) const noexcept {
 	const Node* tmp = Root;
 	while (tmp) {
 		if (tmp->value > val) 
@@ -97,12 +113,30 @@ const T* BinarySearchTree<T>::get(const T& val) const noexcept {
 }
 
 template <typename T>
-BinarySearchTree<T>& BinarySearchTree<T>::operator= (BinarySearchTree<T>&& tree) {
+void BinarySearchTree<T>::Copy(Node* ptr) noexcept {
+	Insert(ptr->value);
+	if (ptr->left) Copy(ptr->left);
+	if (ptr->right) Copy(ptr->right);
+}
+
+template <typename T>
+BinarySearchTree<T>& BinarySearchTree<T>::operator= (const BinarySearchTree<T>& tree) noexcept {
+	if (this == &tree)
+		return *this;
+	
+	delete Root;
+	Size = 0;
+	Copy(tree.Root);
+	return *this;
+}
+
+template <typename T>
+BinarySearchTree<T>& BinarySearchTree<T>::operator= (BinarySearchTree<T>&& tree) noexcept {
 	if (this == &tree)
 		return *this;
 
 	delete Root;
-	size = tree.size;
+	Size = tree.Size;
 	Root = tree.Root;
 	tree.Root = nullptr;
 
@@ -110,44 +144,78 @@ BinarySearchTree<T>& BinarySearchTree<T>::operator= (BinarySearchTree<T>&& tree)
 }
 
 template <typename T>
-BinarySearchTree<T>::~BinarySearchTree() {
-		delete Root;
-		Root = nullptr;
-		size = 0;
+bool BinarySearchTree<T>::Compare(Node* ptr1, Node* ptr2) const noexcept {
+	if (ptr1 && !ptr2) return false;
+	if (!ptr1 && ptr2) return false;
+	if (ptr1 && ptr2) {
+		if (ptr1->value == ptr2->value) {
+			Compare(ptr1->left, ptr2->left);
+			Compare(ptr1->right, ptr2->right);
+		}
+		else return false;
+	}
+	return true;
 }
 
 template <typename T>
-void BinarySearchTree<T>::Traverse(std::ostream& out, Node* root) const noexcept{
+bool BinarySearchTree<T>::operator== (const BinarySearchTree<T>& tree) const noexcept {
+	if (Size != tree.Size) return false;
+	return Compare(Root, tree.Root);
+}
+
+template <typename T>
+BinarySearchTree<T>::~BinarySearchTree() {
+		delete Root;
+		Root = nullptr;
+		Size = 0;
+}
+
+template <typename T>
+void BinarySearchTree<T>::Direct(std::ostream& out, Node* root) const noexcept{
 	if (!root) return;
 	
 	out << root->value << ' ';
-	Traverse(out, root->left);
-	Traverse(out, root->right);
+	Direct(out, root->left);
+	Direct(out, root->right);
+}
+
+template <typename T>
+void BinarySearchTree<T>::Symmetric(std::ostream& out, Node* root) const noexcept {
+	if (!root) return;
+
+	Symmetric(out, root->left);
+	out << root->value << ' ';
+	Symmetric(out, root->right);
 }
 
 template <typename T>
 std::ostream& operator << (std::ostream& out, const BinarySearchTree<T>& tree) {
-	tree.Traverse(out, tree.ReturnRoot());
+	tree.Symmetric(out, tree.ReturnRoot());
 	return out;
 }
 
 template <typename T>
 std::ofstream& operator << (std::ofstream& fout, const BinarySearchTree<T>& tree) {
-	tree.Traverse(fout, tree.ReturnRoot());
+	tree.Direct(fout, tree.ReturnRoot());
 	return fout;
 }
 
 template <typename T>
-std::istream& operator >> (std::istream& in, BinarySearchTree<T>& tree) {
-	size_t size = 0;
-	std::cout << "Enter size: ";
-	std::cin >> size;
-	std::cout << '\n';
-	for (size_t i = 0; i < size; i++) {
-		T value;
-		in >> value;
-		tree.add(value);
-		std::cout << ' ';
-	}
+std::istream& note(std::istream& in, BinarySearchTree<T>& tree) {
+	T value;
+	while (in >> value)
+		tree.Insert(value);
 	return in;
+}
+
+template <typename T>
+std::istream& operator >> (std::istream& in, BinarySearchTree<T>& tree) {
+	note(in, tree);
+	return in;
+}
+
+template <typename T>
+std::ifstream& operator >> (std::ifstream& fin, BinarySearchTree<T>& tree) {
+	note(fin, tree);
+	return fin;
 }
