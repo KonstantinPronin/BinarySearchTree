413d5f282e9e77641a454c6dc5e8685e5f35c6a0
KonstantinPronin <prosayfer@mail.ru>
2016-09-27 21:53:25 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,22 +3,29 @@
 int main() {
 	BinarySearchTree<int> Tree({ 11, 12, 15, 9 });
 	auto a = 8;
-	Tree.add(a);
+	Tree.Insert(a);
 
 	std::ofstream file("C:/Users/sayfer/Desktop/BinarySearchTree.txt");
 	file << Tree;
 	file.close();
-	
-	std::cout << Tree;
-	std::cout << std::endl;
-	
-	BinarySearchTree<int> Tree2(std::move(Tree));
-	std::cout << Tree2;
-	std::cout << std::endl;
 
-	Tree = std::move(Tree2);
-	std::cout << Tree;
+	std::cin >> Tree;
+	std::cout << Tree << std::endl;
+
+	std::ifstream file2("C:/Users/sayfer/Desktop/BinarySearchTree.txt");
+	BinarySearchTree<int> Tree2;
+	file2 >> Tree2;
+	file.close();
+	std::cout << Tree2 << std::endl;
+
+	BinarySearchTree<int> Tree3(std::move(Tree));
+	std::cout << Tree3 << std::endl;
+
+	Tree = Tree3;
+	std::cout << Tree << std::endl;
 	
+	std::cout << (Tree == Tree2) << std::endl;
+
 	system("pause");
 	return 0;
 }
