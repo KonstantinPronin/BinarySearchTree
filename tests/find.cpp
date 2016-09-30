429b0614048fabe5f6c9a53f3910c0e0afb01fe0
KonstantinPronin <prosayfer@mail.ru>
2016-09-30 17:46:49 +0400

    Update find.cpp

--- tests/find.cpp
+++ tests/find.cpp
@@ -1,1 +1,54 @@
 
+#include "Catch.h"
+#include "BinarySearchTree.h"
+
+SCENARIO ("Size of empty tree") {
+    GIVEN("Given empty tree") {
+        BinarySearchTree<int> tree;
+
+       WHEN ("Decide size") {
+            THEN ("Size must be empty") {
+                REQUIRE(tree.size() == 0);
+            }
+        }
+    }
+}
+
+SCENARIO("Size of tree with some itemps"){
+    GIVEN("Given not empty tree") {
+        BinarySearchTree<int> tree{11, 14, 16, 18};
+
+        WHEN ("Decide size") {
+            THEN ("Size mustn`t be empty") {
+                REQUIRE(tree.size() != 0);
+            }
+        }
+    }
+
+}
+
+SCENARIO("Size of empty const tree"){
+    GIVEN("Given const empty tree") {
+        const BinarySearchTree<int> tree;
+
+        WHEN ("Decide size") {
+            THEN ("Size must be empty") {
+                REQUIRE(tree.size() == 0);
+            }
+        }
+    }
+
+}
+
+SCENARIO("Size of not empty const tree"){
+    GIVEN("Given const not empty tree") {
+        const BinarySearchTree<int> tree;
+
+        WHEN ("Decide size") {
+            THEN ("Size mustn`t be empty") {
+                REQUIRE(tree.size() != 0);
+            }
+        }
+    }
+
+}
