7e44d208a58774bf43beeb98231e7d2e513badc2
KonstantinPronin <prosayfer@mail.ru>
2016-09-30 19:12:08 +0400

    Update insert.cpp

--- tests/insert.cpp
+++ tests/insert.cpp
@@ -2,31 +2,33 @@
 #include "../tests/catch.h"
 #include "..include/BinarySearchTree.h"
 
-SCENARIO("If inserting element already exist in tree, method must return false")
+SCENARIO("If inserting element already exist in tree")
 {
    GIVEN("Tree with existing element")
    {
       BinarySearchTree<int> tree {8,10,3};
       WHEN("Inserting")
       {
-         THEN("Method must return false")
+         THEN("Method must return false and doent change size")
          {
             REQUIRE(tree.insert(10) == false);
+            REQUIRE(tree.size() == 3)
          }
       }
    }
 }
 
-SCENARIO("If inserting element does not exist in tree, method must return true")
+SCENARIO("If inserting element does not exist in tree")
 {
    GIVEN("Tree with non-existing element")
    {
       BinarySearchTree<int> tree { 8,10,3};
       WHEN("Inserting")
       {
-         THEN("Method must return true")
+         THEN("Method must return true and change size")
          {
             REQUIRE(tree.insert(7) == true);
+            REQUIRE(tree.size() == 4)
          }
       }
    }
