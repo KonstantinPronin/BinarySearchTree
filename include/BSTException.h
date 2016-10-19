c3e4bebb476a91e9399b76c9a622be0d0ece2645
KonstantinPronin <prosayfer@mail.ru>
2016-10-19 18:09:14 +0300

    Update BSTException.h

--- include/BSTException.h
+++ include/BSTException.h
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <exception>
-#include <string>
 
 template <typename T>
 class BST_logic_error:public std::logic_error{
 public:
     explicit BST_logic_error(const char* what_arg):std::logic_error(what_arg){}
-    std::string what(){
+    const char* what(){
         return std::logic_error::what();
     }
 };
